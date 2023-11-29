cccdddb788dd4c4dd7ae53529aaa44e3e1f25ff7
Alexander Urbina mar <aurbina@hiper.com.pe>
2023-11-29 00:29:49 -0500

    change bootloaders name

--- product/src/main/java/org/sagapattern/product/RefactorBoot.java
+++ product/src/main/java/org/sagapattern/product/RefactorBoot.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class RefactorBoot {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(RefactorBoot.class, args);
     }
 }
